f05e6923bc9783e560300d9220727349d547ed0b
Job Moreno <jobmoreno98@gmail.com>
2025-10-04 22:47:53 -0600

    se quita webpack

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @include('layouts.head')
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+
 
 </head>
 
